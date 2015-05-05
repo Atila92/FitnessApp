02fad6324fb0fbd3a5da29b902e9dfacc5234bbe
Atila <atila_sarvari@hotmail.com>
2015-05-05 17:18:23 +0200

    alt

--- app/src/main/java/com/example/atila/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/atila/fitnessapp/MainActivity.java
@@ -15,94 +15,36 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final String TAG = "MainActivity";
-
-    private GoogleCommunicator mGCom;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.v(TAG, "- onCreate -");
-
-        doSomeGoogleStuff();
-
     }
 
-    private void doSomeGoogleStuff(){
-        mGCom = new GoogleCommunicator(this,"atila.sarvari@gmail.com");
-        mGCom.setupFeed("my_spreadsheet","my_worksheet");
-    }
-
+    // Called to lazily initialize the action bar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+    // Called every time user clicks on an action
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            case R.id.//timershit:
+                startActivity(new Intent("com.marakana.android.yamba.action.tweet"));
+                return true;
+            case R.id.action_refresh:
+                startService(new Intent(this, RefreshService.class));
+                return true;
+            default:
+                return false;
 
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * This method is a hook for background threads and async tasks that need to provide the
-     * user a response UI when an exception occurs.
-     */
-    static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
-    static final int REQUEST_CODE_RECOVER_FROM_AUTH_ERROR = 1001;
-    static final int REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR = 1002;
-    public void handleException(final Exception e) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (e instanceof GooglePlayServicesAvailabilityException) {
-                    // The Google Play services APK is old, disabled, or not present.
-                    // Show a dialog created by Google Play services that allows
-                    // the user to update the APK
-                    int statusCode = ((GooglePlayServicesAvailabilityException)e)
-                            .getConnectionStatusCode();
-                    Dialog dialog = GooglePlayServicesUtil.getErrorDialog(statusCode,
-                            MainActivity.this,
-                            REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
-                    dialog.show();
-                } else if (e instanceof UserRecoverableAuthException) {
-                    // Unable to authenticate, such as when the user has not yet granted
-                    // the app access to the account, but the user can fix this.
-                    // Forward the user to an activity in Google Play services.
-                    Intent intent = ((UserRecoverableAuthException)e).getIntent();
-                    startActivityForResult(intent,
-                            REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if ((requestCode == REQUEST_CODE_RECOVER_FROM_AUTH_ERROR ||
-                requestCode == REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR)
-                && resultCode == RESULT_OK) {
-
-
-            Log.i(TAG, "Retrying");
-            doSomeGoogleStuff();
-
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
     }
 }
